d5fb3ddbc6774dbdbda5ab960dd339bb0cbd808d
Farah Elkourdi <felkourd@stevens.edu>
2022-04-22 19:24:55 -0400

    project structure :hammer:

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express();
 const static = express.static(__dirname + '/public');
-
+const session = require('express-session');
 const configRoutes = require('./routes');
+const cookieParser = require("cookie-parser");
 const exphbs = require('express-handlebars');
 
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(
+  session({
+    name: 'AuthCookie',
+    secret: 'some secret string!',
+    resave: false,
+    saveUninitialized: true  
+  })
+);
 
 configRoutes(app);
 
