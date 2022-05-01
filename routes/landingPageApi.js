902503a464611222b57a059c12e7427c263c55ab
Avinash Kumar <akumar40@stevens.edu>
2022-05-01 01:06:36 -0400

    Updated CSS for login and signup
    1. Updated CSS for login and signup
    2. Changes landing page event to get all events
    3. Events fetched start date to be greater than current date time

--- routes/landingPageApi.js
+++ routes/landingPageApi.js
@@ -6,12 +6,7 @@ const gameEvent = require('../data/gameEvent');
 // Router configuration
 
 router.get('/', async (req, res) => {
-    let defaultArea = 'hoboken';
-    // TO-DO show list of events near user area
-    if (req.session.user) {
-        defaultArea = req.session.user.userArea;
-    }
-    let events = await gameEvent.getGameEventbyAreaLimit(defaultArea, 5);
+    let events = await gameEvent.getGameEventLandingPage();
     if (events) {
         const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
         events.forEach(event => {
