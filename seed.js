2a42cc5438e01a350ddc58dd24d75668c1aab1aa
HarshSinghania-07 <hsharsh.singhania@gmail.com>
2022-05-08 13:32:51 -0400

    Seed file [Rating left]

--- seed.js
+++ seed.js
@@ -2,6 +2,7 @@ const connection = require('./config/mongoConnection');
 const data = require("./data");
 const eventData = require("./data/gameEvent");
 const userData = require("./userData");
+const commentData = require("./data/comments");
 
 const { ObjectId } = require("mongodb");
 
@@ -21,6 +22,10 @@ async function main(){
     let baseballEvent = undefined;
     let basketballEvent = undefined;
 
+    let comment1 = undefined;
+    let comment2 = undefined;
+    let comment3 = undefined;
+
     try{
         user1 = await userData.createUser(
             "John",
@@ -32,7 +37,7 @@ async function main(){
             "40.7365224",
             "-74.0311785"
         );
-        console.log(user1);
+        // console.log(user1);
     } catch (e){
         console.log(e);
     }
@@ -48,26 +53,26 @@ async function main(){
             "40.7457215",
             "-74.0482442"
         );
-        console.log(user2);
+        // console.log(user2);
     } catch (e){
         console.log(e);
     }
 
-    // try{
-    //     user3 = await userData.createUser(
-    //         "Avinash",
-    //         "Kumar",
-    //         "avinashk@gmail.com",
-    //         "avinash@123",
-    //         "6th Street",
-    //         "Hoboken",
-    //         "40",
-    //         "-70"
-    //     );
-    //     console.log(user2);
-    // } catch (e){
-    //     console.log(e);
-    // }
+    try{
+        user3 = await userData.createUser(
+            "avinash",
+            "kumar",
+            "avinashk@gmail.com",
+            "avinash@123",
+            "bowers street",
+            "jersey city",
+            "40.7482695",
+            "-74.0509703"
+        );
+        // console.log(user3);
+    } catch (e){
+        console.log(e);
+    }
 
     try{
         soccerEvent = await eventData.create(
@@ -85,7 +90,7 @@ async function main(){
             10,
             14
         );
-        console.log(soccerEvent);
+        // console.log(soccerEvent);
     }catch(e){
         console.log(e);
     }
@@ -106,29 +111,47 @@ async function main(){
             8,
             12
         );
-        console.log(soccerEvent);
+        // console.log(basketballEvent);
     }catch(e){
         console.log(e);
     }
 
+    try{
+        frisbeeEvent = await eventData.create(
+            user3._id,
+            "Frisbee event",
+            "upcoming",
+            "frisbee",
+            "playing frisbee in jersey city",
+            "jersey city",
+            "Pershing Field",
+            "40.742262",
+            "-74.05347075926747",
+            "2022-08-10T20:00:00.000+00:00",
+            "2022-08-10T22:00:00.000+00:00",
+            4,
+            8
+        );
+        // console.log(frisbeeEvent);
+    }catch(e){
+        console.log(e);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    try{
+        comment1 = await commentData.postComment(
+            user1._id,
+            soccerEvent._id,
+            "Looking forward to play soccer with everyone!",
+            "2022-05-08T17:30:25.363+00:00",
+            "johndoe@gmail.com"
+        );
+        // console.log(comment1);
+    }catch(e){
+        console.log(e);
+    }
 
     await connection.closeConnection();
 }
-
-
 
 
 main();
