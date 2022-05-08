c77aefd30b7b2af98d6b7dff3ecdfcae63f4b2c4
Farah Elkourdi <felkourd@stevens.edu>
2022-05-08 02:06:21 -0400

    The Extra feature :sparkles:

--- data/contactus.js
+++ data/contactus.js
@@ -93,7 +93,7 @@ async emailSetup2( title, action, email) {
   );
   const mailOptions = {
     from: process.env.UserEmail,
-    to: process.env.UserEmail,
+    to: email,
     subject: "Notification",
     template: 'notification',
     context: {
